7ec4b65c00fc3065d5b699d853387f5f102d6823
mino-su <omsalstn0401@gmail.com>
2025-08-21 17:56:44 +0900

    [#7] 테이블 스키마 생성 : 25.08.21

---
+++ src/main/java/org/delivery/koreatestdata/domain/TableSchema.java
@@ -0,0 +1,48 @@
+package org.delivery.koreatestdata.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import org.hibernate.proxy.HibernateProxy;
+
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+@Getter
+@NoArgsConstructor
+@Entity
+@ToString
+@Table
+public class TableSchema {
+    private String schemaName;
+
+    @Id
+    @GeneratedValue()
+    private String userId;
+
+    private LocalDateTime exportedAt;
+
+    public boolean isExported() {
+        return exportedAt != null;
+    }
+
+    @Override
+    public final boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null) return false;
+        Class<?> oEffectiveClass = o instanceof HibernateProxy ? ((HibernateProxy) o).getHibernateLazyInitializer().getPersistentClass() : o.getClass();
+        Class<?> thisEffectiveClass = this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass() : this.getClass();
+        if (thisEffectiveClass != oEffectiveClass) return false;
+        TableSchema that = (TableSchema) o;
+        return getUserId() != null && Objects.equals(getUserId(), that.getUserId());
+    }
+
+    @Override
+    public final int hashCode() {
+        return this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass().hashCode() : getClass().hashCode();
+    }
+}
