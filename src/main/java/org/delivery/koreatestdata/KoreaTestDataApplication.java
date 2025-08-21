6f831170be08d24da0575e2823ee72f957b01a20
mino-su <omdalstn0401@gmail.com>
2025-08-21 16:14:33 +0900

    [#1] spring boot 생성

---
+++ src/main/java/org/delivery/koreatestdata/KoreaTestDataApplication.java
@@ -0,0 +1,13 @@
+package org.delivery.koreatestdata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KoreaTestDataApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KoreaTestDataApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/delivery/koreatestdata/KoreaTestDataApplicationTests.java
@@ -0,0 +1,13 @@
+package org.delivery.koreatestdata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KoreaTestDataApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
