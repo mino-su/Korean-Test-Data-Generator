da3cfeaf07208f8243533dce8d6df5e3024ec86d
mino-su <omsalstn0401@gmail.com>
2025-09-02 15:08:26 +0900

    [#10] 테스트용 인메모리 DB : 25.09.02 - H2 프로퍼티 추가

---
+++ src/main/java/org/delivery/koreatestdata/domain/MockData.java
@@ -0,0 +1,13 @@
+package org.delivery.koreatestdata.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class MockData {
+    private String mockDataType;
+    private String mockDataValue;
+}
